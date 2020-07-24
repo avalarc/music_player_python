5af430a7826ebcfc0bd2b7245d128ad84a569b3e
avalarc <avaldes@nadhis.com>
2020-07-24 15:20:02 -0500

    Agregando funcion Reproducir

--- Reproductor.py
+++ Reproductor.py
@@ -33,7 +33,7 @@ class ReproductorMusical:
         contenedorBotones = LabelFrame(self.container,text="Control Panel",font=("fonatica",15,"bold"),bg="grey",fg="white",bd=5,relief=FLAT)
         contenedorBotones.place(x=600,y=500,width=400,height=100)
         # Boton de play
-        playBtn = Button(contenedorBotones,text="Play",command=self.play,width=6,height=1,font=("fonatica",12),fg="black",bg="grey").grid(row=0,column=0,padx=10,pady=5)
+        playBtn = Button(contenedorBotones,text="Play",command=self.ReproducirCancion,width=6,height=1,font=("fonatica",12),fg="black",bg="grey").grid(row=0,column=0,padx=10,pady=5)
         # Boton de Pause
         pausaOnBtn = Button(contenedorBotones,text="Pause On",command=self.pause,width=8,height=1,font=("fonatica",12),fg="black",bg="grey").grid(row=0,column=1,padx=10,pady=5)
         # Boton de UnPause
@@ -59,6 +59,17 @@ class ReproductorMusical:
         # Insertando canciones en el playlist
         for track in songtracks:
             self.playlist.insert(END,track)
+        
+    # Definiendo las funciones para la ejecucion de las canciones
+    def ReproducirCancion(self):
+        # Displaying Selected Song title
+        self.cancion.set(self.playlist.get(ACTIVE))
+        # Displaying Status
+        self.estado.set("-Playing")
+        # Loading Selected Song
+        pygame.mixer.music.load(self.playlist.get(ACTIVE))
+        # Playing Selected Song
+        pygame.mixer.music.play()
        
         
 # Creating TK Container
